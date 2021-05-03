d5f24b3f010f706624c0a59942c814ae13b99d45
spectocr <highstakes22@gmail.com>
2021-05-03 15:58:35 -0400

    finished 4.2.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,23 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 var creatTaskHandler = function(event) { //i don't understand the event being placed here as a parameter, yet it doesn't get called until after.
 
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //creat list item
     var lisItemEl = document.createElement("li");
     lisItemEl.className = "task-item";
-    lisItemEl.textContent = "This is a new task.";
+
+    //creat div to hold task inof and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    lisItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     taskToDoEl.appendChild(lisItemEl);
 };
 
