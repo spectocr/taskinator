ef305bf9254d51939672a4cf8029aa407660bff9
spectocr <highstakes22@gmail.com>
2021-05-03 16:43:23 -0400

    yass done 4.2.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-var creatTaskHandler = function(event) { //i don't understand the event being placed here as a parameter, yet it doesn't get called until after.
+var taskFormHandler = function(event) { //i don't understand the event being placed here as a parameter, yet it doesn't get called until after.
 
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to creatTaskEl
+    createTaskEl(taskDataObj); // ***** if createTaskEl is after it being called, why isn't there an error? I thoguht you have to define the var/function before you called them?
+
+};
+
+var createTaskEl = function(taskDataObj) {
     //creat list item
     var lisItemEl = document.createElement("li");
     lisItemEl.className = "task-item";
@@ -16,12 +28,12 @@ var creatTaskHandler = function(event) { //i don't understand the event being pl
     // give it a class name
     taskInfoEl.className = "task-info";
     //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     lisItemEl.appendChild(taskInfoEl);
 
     //add entire list item to list
     taskToDoEl.appendChild(lisItemEl);
-};
+}
 
-formEl.addEventListener("submit", creatTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
