acc57b227775c90aa2a1d2f8f5bc99b7f92da5d0
spectocr <highstakes22@gmail.com>
2021-05-03 15:19:27 -0400

    adding the stuffs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-var creatTaskHandler = function() {
+var creatTaskHandler = function(event) { //i don't understand the event being placed here as a parameter, yet it doesn't get called until after.
+
+    event.preventDefault();
+
     var lisItemEl = document.createElement("li");
     lisItemEl.className = "task-item";
     lisItemEl.textContent = "This is a new task.";
     taskToDoEl.appendChild(lisItemEl);
-}
+};
 
-buttonEl.addEventListener("click", creatTaskHandler);
+formEl.addEventListener("submit", creatTaskHandler);
